f11e014a18dba2b1b9dbce5dbecf75c1e402fe82
Hendrik Ranocha <h.ranocha@tu-bs.de>
2017-10-25 16:03:35 +0200

    generalised Laguerre polynomials

--- src/laguerre.jl
+++ src/laguerre.jl
@@ -20,3 +20,27 @@ function laguerre(x, p::Integer)
 
     p₁
 end
+
+"""
+    laguerre(x, p::Integer, α)
+
+Evaluate the generalised Laguerre polynomial with parameter `α` of degree `p`
+at `x` using the three term recursion.
+"""
+function laguerre(x, p::Integer, α)
+    T = typeof( α+1-x )
+    p₀ = one(T)
+    p₁ = α+1-x
+
+    if p <= 0
+        return p₀
+    elseif p == 1
+        return p₁
+    end
+
+    for n in 2:p
+        p₀, p₁ = p₁, ( (α+2n-1-x)*p₁ - (α+n-1)*p₀ ) / n
+    end
+
+    p₁
+end
--- test/laguerre_test.jl
+++ test/laguerre_test.jl
@@ -1,7 +1,7 @@
 using Base.Test, PolynomialBases
 import SymPy
 
-x = SymPy.symbols("x")
+x, α = SymPy.symbols("x, alpha")
 
 @test 0 == SymPy.simplify( laguerre(x, 0) - 1 )
 @test 0 == SymPy.simplify( laguerre(x, 1) - ( 1-x ) )
@@ -11,5 +11,19 @@ x = SymPy.symbols("x")
 @test 0 == SymPy.simplify( laguerre(x, 5) - ( -x^5 + 25x^4 - 200x^3 + 600x^2 - 600x + 120 ) / 120 )
 @test 0 == SymPy.simplify( laguerre(x, 6) - ( x^6 - 36x^5 + 450x^4 - 2400x^3 + 5400x^2 - 4320x + 720 ) / 720 )
 
+@test 0 == SymPy.simplify( laguerre(x, 0, α) - 1 )
+# https://www.wolframalpha.com/input/?i=LaguerreL%5B1,+alpha,+x%5D
+@test 0 == SymPy.simplify( laguerre(x, 1, α) - ( α-x+1 ) )
+# https://www.wolframalpha.com/input/?i=LaguerreL%5B2,+alpha,+x%5D
+@test 0 == SymPy.simplify( laguerre(x, 2, α) - ( α^2 + 3α + x^2 - 2α*x - 4x + 2 ) / 2 )
+# https://www.wolframalpha.com/input/?i=LaguerreL%5B3,+alpha,+x%5D
+@test 0 == SymPy.simplify( laguerre(x, 3, α) - ( α^3+6α^2+11α-x^3+3α*x^2+9x^2-3α^2*x-15α*x-18x+6 ) / 6 )
+# https://www.wolframalpha.com/input/?i=LaguerreL%5B4,+alpha,+x%5D
+@test 0 == SymPy.simplify( laguerre(x, 4, α) - ( α^4+10α^3+35α^2+50α+x^4-4α*x^3-16x^3+6α^2*x^2+42α*x^2+72x^2-4α^3*x-36α^2*x-104α*x-96x+24 ) / 24 )
+
 @inferred laguerre(x, 4)
+@inferred laguerre(x, 4, α)
+@inferred laguerre(x, 4, 2)
 @inferred laguerre(10., 4)
+@inferred laguerre(10., 4, 2.)
+@inferred laguerre(10., 4, 2)
