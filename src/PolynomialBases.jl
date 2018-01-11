84158c61340703d17b05a4d2bfcc6eec376e8f36
Hendrik Ranocha <h.ranocha@tu-bs.de>
2018-01-11 08:39:33 +0100

    added precompilation

--- src/PolynomialBases.jl
+++ src/PolynomialBases.jl
@@ -1,11 +1,13 @@
-module PolynomialBases
+__precompile__()
 
+module PolynomialBases
 
 using Requires
 using Parameters
 using FastGaussQuadrature
 
 
+# types
 abstract type AbstractDomain{Dim} end
 struct Line <: AbstractDomain{1} end
 
@@ -13,6 +15,7 @@ abstract type AbstractBasis{Domain} end
 abstract type NodalBasis{Domain} <: AbstractBasis{Domain} end
 
 
+# source files
 include("canonical_mappings.jl")
 include("interpolation.jl")
 include("integration.jl")
@@ -25,33 +28,34 @@ include("hermite.jl")
 include("laguerre.jl")
 include("hahn.jl")
 
-# types
+# export
+## types
 export NodalBasis, LobattoLegendre, GaussLegendre, GaussJacobi
 
-# mappings
+## mappings
 export map_to_canonical, map_to_canonical!, map_from_canonical, map_from_canonical!
 
-# interpolation
+## interpolation
 export interpolate, interpolate!, interpolation_matrix, interpolation_matrix!,
         change_basis, change_basis!
 
-# derivative
+## derivative
 export derivative_at, derivative_at!, derivative_matrix, derivative_matrix!
 
-# integration
+## integration
 export integrate
 
-# Continuous orthogonal polynomials: Legendre, Gegenbauer, Jacobi, Hermite
+## Continuous orthogonal polynomials: Legendre, Gegenbauer, Jacobi, Hermite
 export legendre, legendre_vandermonde, legendre_D, legendre_M,
        gegenbauer,
        jacobi, jacobi_vandermonde, jacobi_M,
        hermite,
        laguerre
 
-# Discrete orthogonal polynomials: Hahn
+## Discrete orthogonal polynomials: Hahn
 export hahn
 
-# other utilities
+## other utilities
 export utility_matrices
 
 
