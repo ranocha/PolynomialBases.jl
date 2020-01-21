03e63120b59793ca04f0af3fa7256acd625c9d57
Hendrik Ranocha <mail@ranocha.de>
2020-01-21 09:46:46 +0300

    skip some sympy tests on Julia NewPkgEval

--- test/hahn_test.jl
+++ test/hahn_test.jl
@@ -1,31 +1,33 @@
 using Test, PolynomialBases
-import SymPy
 
-x, α, β, N = SymPy.symbols("x, alpha, beta, N")
+if !haskey(ENV, "JULIA_PKGEVAL") # sympy is not install on https://github.com/JuliaComputing/NewPkgEval.jl
+    import SymPy
+    x, α, β, N = SymPy.symbols("x, alpha, beta, N")
 
-@test 0 == SymPy.simplify( hahn(x, 0, α, β, N) - 1 )
-# https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-1,+1%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
-@test 0 == SymPy.simplify( hahn(x, 1, α, β, N) - ( 1 - (α+β+2)*x/(N*(α+1)) ) )
-# https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-2,+2%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
-@test 0 == SymPy.simplify( hahn(x, 2, α, β, N) - (
-        (1-x)*x*(α+β+4)*(α+β+3) / ((α+1)*(α+2)*(1-N)*N)
-        - 2x*(α+β+3)/(N*(α+1)) + 1
-    ) )
-# https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-3,+3%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
-@test 0 == SymPy.simplify( hahn(x, 3, α, β, N) - (
-        3*(1-x)*x*(α+β+5)*(α+β+4) / ( (α+1)*(α+2)*N*(1-N) )
-        - (1-x)*(2-x)*x*(α+β+5)*(α+β+6)*(α+β+4) / ( (α+1)*(α+2)*(α+3)*N*(1-N)*(2-N) )
-        - 3x*(α+β+4) / ( N*(α+1) ) + 1
-    ) )
-# https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-4,+4%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
-@test 0 == SymPy.simplify( hahn(x, 4, α, β, N) - (
-        6*(1-x)*x*(α+β+6)*(α+β+5) / ( (α+1)*(α+2)*N*(1-N) )
-        - 4*(1-x)*(2-x)*x*(α+β+6)*(α+β+7)*(α+β+5) / ( (α+1)*(α+2)*(α+3)*N*(1-N)*(2-N) )
-        + (1-x)*(2-x)*(3-x)*x*(α+β+6)*(α+β+7)*(α+β+8)*(α+β+5) / ( (α+1)*(α+2)*(α+3)*(α+4)*N*(1-N)*(2-N)*(3-N) )
-        - 4x*(α+β+5) / ( N*(α+1) )
-        + 1
-    ) )
+    @test 0 == SymPy.simplify( hahn(x, 0, α, β, N) - 1 )
+    # https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-1,+1%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
+    @test 0 == SymPy.simplify( hahn(x, 1, α, β, N) - ( 1 - (α+β+2)*x/(N*(α+1)) ) )
+    # https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-2,+2%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
+    @test 0 == SymPy.simplify( hahn(x, 2, α, β, N) - (
+            (1-x)*x*(α+β+4)*(α+β+3) / ((α+1)*(α+2)*(1-N)*N)
+            - 2x*(α+β+3)/(N*(α+1)) + 1
+        ) )
+    # https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-3,+3%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
+    @test 0 == SymPy.simplify( hahn(x, 3, α, β, N) - (
+            3*(1-x)*x*(α+β+5)*(α+β+4) / ( (α+1)*(α+2)*N*(1-N) )
+            - (1-x)*(2-x)*x*(α+β+5)*(α+β+6)*(α+β+4) / ( (α+1)*(α+2)*(α+3)*N*(1-N)*(2-N) )
+            - 3x*(α+β+4) / ( N*(α+1) ) + 1
+        ) )
+    # https://www.wolframalpha.com/input/?i=HypergeometricPFQ%5B+%7B-4,+4%2Balpha%2Bbeta%2B1,+-x%7D,+%7Balpha%2B1,+-N%7D,+1%5D
+    @test 0 == SymPy.simplify( hahn(x, 4, α, β, N) - (
+            6*(1-x)*x*(α+β+6)*(α+β+5) / ( (α+1)*(α+2)*N*(1-N) )
+            - 4*(1-x)*(2-x)*x*(α+β+6)*(α+β+7)*(α+β+5) / ( (α+1)*(α+2)*(α+3)*N*(1-N)*(2-N) )
+            + (1-x)*(2-x)*(3-x)*x*(α+β+6)*(α+β+7)*(α+β+8)*(α+β+5) / ( (α+1)*(α+2)*(α+3)*(α+4)*N*(1-N)*(2-N)*(3-N) )
+            - 4x*(α+β+5) / ( N*(α+1) )
+            + 1
+        ) )
 
-@test_throws ArgumentError hahn(x, 6, α, β, 5)
-@test_skip hahn(x, 4, α, β, 5)
+    @test_throws ArgumentError hahn(x, 6, α, β, 5)
+    @test_skip hahn(x, 4, α, β, 5)
+end
 @inferred hahn(10., 4, -0.5, -0.9, 5)
--- test/sympy_test.jl
+++ test/sympy_test.jl
@@ -1,27 +1,30 @@
 using Test, PolynomialBases
-import SymPy
 
-tol = 5.e-15
+if !haskey(ENV, "JULIA_PKGEVAL") # sympy is not install on https://github.com/JuliaComputing/NewPkgEval.jl
+  import SymPy
 
-for p in 0:6
-    basis_sympy = LobattoLegendre(p, SymPy.Sym)
-    basis_float = LobattoLegendre(p, Float64)
-    @test maximum(abs.( float.(basis_sympy.nodes) - basis_float.nodes )) < tol
-    @test maximum(abs.( float.(basis_sympy.weights) - basis_float.weights )) < tol
-    @test maximum(abs.( float.(basis_sympy.baryweights) - basis_float.baryweights )) < tol
-    @test maximum(abs.( float.(basis_sympy.D) - basis_float.D, )) < 2tol
+  tol = 5.e-15
+
+  for p in 0:6
+      basis_sympy = LobattoLegendre(p, SymPy.Sym)
+      basis_float = LobattoLegendre(p, Float64)
+      @test maximum(abs.( float.(basis_sympy.nodes) - basis_float.nodes )) < tol
+      @test maximum(abs.( float.(basis_sympy.weights) - basis_float.weights )) < tol
+      @test maximum(abs.( float.(basis_sympy.baryweights) - basis_float.baryweights )) < tol
+      @test maximum(abs.( float.(basis_sympy.D) - basis_float.D, )) < 2tol
+  end
+  @test_throws ArgumentError LobattoLegendre(7, SymPy.Sym)
+
+  for p in 0:4
+      basis_sympy = GaussLegendre(p, SymPy.Sym)
+      basis_float = GaussLegendre(p, Float64)
+      @test maximum(abs.( float.(basis_sympy.nodes) - basis_float.nodes )) < tol
+      @test maximum(abs.( float.(basis_sympy.weights) - basis_float.weights )) < tol
+      @test maximum(abs.( float.(basis_sympy.baryweights) - basis_float.baryweights )) < tol
+      @test maximum(abs.( float.(basis_sympy.D) - basis_float.D, )) < 2tol
+  end
+  @test_throws ArgumentError GaussLegendre(5, SymPy.Sym)
+
+  interpolation_matrix([-1, 1], LobattoLegendre(4, SymPy.Sym))
+  interpolation_matrix([-1, 1], GaussLegendre(4, SymPy.Sym))
 end
-@test_throws ArgumentError LobattoLegendre(7, SymPy.Sym)
-
-for p in 0:4
-    basis_sympy = GaussLegendre(p, SymPy.Sym)
-    basis_float = GaussLegendre(p, Float64)
-    @test maximum(abs.( float.(basis_sympy.nodes) - basis_float.nodes )) < tol
-    @test maximum(abs.( float.(basis_sympy.weights) - basis_float.weights )) < tol
-    @test maximum(abs.( float.(basis_sympy.baryweights) - basis_float.baryweights )) < tol
-    @test maximum(abs.( float.(basis_sympy.D) - basis_float.D, )) < 2tol
-end
-@test_throws ArgumentError GaussLegendre(5, SymPy.Sym)
-
-interpolation_matrix([-1, 1], LobattoLegendre(4, SymPy.Sym))
-interpolation_matrix([-1, 1], GaussLegendre(4, SymPy.Sym))
