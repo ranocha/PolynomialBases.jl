c20acef54dc300b879965953e99e89d23b19bc60
Hendrik Ranocha <mail@ranocha.de>
2023-05-09 08:39:53 +0200

    fix typos

--- test/symengine_test.jl
+++ test/symengine_test.jl
@@ -4,22 +4,22 @@ import SymEngine
 tol = 5.e-14
 
 for p in 0:6
-    basis_symbo = LobattoLegendre(p, SymEngine.Basic)
-    basis_float = LobattoLegendre(p, Float64)
-    @test maximum(abs.( float.(basis_symbo.nodes) - basis_float.nodes )) < tol
-    @test maximum(abs.( float.(basis_symbo.weights) - basis_float.weights )) < tol
-    @test maximum(abs.( float.(basis_symbo.baryweights) - basis_float.baryweights )) < tol
-    @test maximum(abs.( float.(basis_symbo.D) - basis_float.D, )) < tol
+    basis_symbolic = LobattoLegendre(p, SymEngine.Basic)
+    basis_float    = LobattoLegendre(p, Float64)
+    @test maximum(abs.( float.(basis_symbolic.nodes) - basis_float.nodes )) < tol
+    @test maximum(abs.( float.(basis_symbolic.weights) - basis_float.weights )) < tol
+    @test maximum(abs.( float.(basis_symbolic.baryweights) - basis_float.baryweights )) < tol
+    @test maximum(abs.( float.(basis_symbolic.D) - basis_float.D, )) < tol
 end
 @test_throws ArgumentError LobattoLegendre(7, SymEngine.Basic)
 
 for p in 0:4
-    basis_symbo = GaussLegendre(p, SymEngine.Basic)
+    basis_symbolic = GaussLegendre(p, SymEngine.Basic)
     basis_float = GaussLegendre(p, Float64)
-    @test maximum(abs.( float.(basis_symbo.nodes) - basis_float.nodes )) < tol
-    @test maximum(abs.( float.(basis_symbo.weights) - basis_float.weights )) < tol
-    @test maximum(abs.( float.(basis_symbo.baryweights) - basis_float.baryweights )) < tol
-    @test maximum(abs.( float.(basis_symbo.D) - basis_float.D, )) < tol
+    @test maximum(abs.( float.(basis_symbolic.nodes) - basis_float.nodes )) < tol
+    @test maximum(abs.( float.(basis_symbolic.weights) - basis_float.weights )) < tol
+    @test maximum(abs.( float.(basis_symbolic.baryweights) - basis_float.baryweights )) < tol
+    @test maximum(abs.( float.(basis_symbolic.D) - basis_float.D, )) < tol
 end
 @test_throws ArgumentError GaussLegendre(5, SymEngine.Basic)
 
