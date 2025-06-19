fae199c61a0b771b165d8a6dff58f04862c77fd4
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-06-19 14:48:46 +0200

    add GaussRadau basis (#104)
    * add GaussRadau basis
    
    * fix left node for p = 0
    
    * bump compat of FastGaussQuadrature.jl to v0.4.3
    
    * also add GaussRadauRight
    
    * fix indentation
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/nodal_bases.jl
+++ src/nodal_bases.jl
@@ -184,6 +184,8 @@ function Base.show(io::IO, basis::LobattoLegendre{T}) where {T}
 end
 
 @inline includes_boundaries(basis::LobattoLegendre) = Val{true}()
+@inline includes_left_boundary(basis::LobattoLegendre) = Val{true}()
+@inline includes_right_boundary(basis::LobattoLegendre) = Val{true}()
 
 @inline satisfies_sbp(basis::LobattoLegendre) = Val{true}()
 
@@ -239,8 +241,116 @@ function Base.show(io::IO, basis::GaussLegendre{T}) where {T}
 end
 
 @inline includes_boundaries(basis::GaussLegendre) = Val{false}()
+@inline includes_left_boundary(basis::GaussLegendre) = Val{false}()
+@inline includes_right_boundary(basis::GaussLegendre) = Val{false}()
 
 @inline satisfies_sbp(basis::GaussLegendre) = Val{true}()
+
+
+"""
+    GaussRadauLeft{T}
+
+The nodal basis corresponding to Radau Gauss quadrature in [-1,1]
+including the left end point with scalar type `T`.
+"""
+@auto_hash_equals struct GaussRadauLeft{T} <: NodalBasis{Line}
+    nodes::Vector{T}
+    weights::Vector{T}
+    baryweights::Vector{T}
+    D::Matrix{T}
+    interp_left::Vector{T}
+    interp_right::Vector{T}
+
+    function GaussRadauLeft(nodes::Vector{T}, weights::Vector{T}, baryweights::Vector{T}, D::Matrix{T}, interp_left::Vector{T}, interp_right::Vector{T}) where {T}
+        @argcheck length(nodes) == length(weights) == length(baryweights) == size(D,1) == size(D,2) == length(interp_left) == length(interp_right)
+        new{T}(nodes, weights, baryweights, D, interp_left, interp_right)
+    end
+end
+
+"""
+    GaussRadauLeft(p::Int, T=Float64)
+
+Generate the `GaussRadauLeft` basis of degree `p` with scalar type `T`.
+"""
+function GaussRadauLeft(p::Int, T=Float64)
+    nodes, weights = gauss_radau_nodes_and_weights_impl(p, T)
+    baryweights = barycentric_weights(nodes)
+    D = derivative_matrix(nodes, baryweights)
+    R = interpolation_matrix(T[-1, 1], nodes, baryweights)
+    GaussRadauLeft(nodes, weights, baryweights, D, R[1,:], R[2,:])
+end
+
+function gauss_radau_nodes_and_weights_impl(p, T::DataType)
+    nodes::Vector{T}, weights::Vector{T} = gaussradau(p+1, T)
+    nodes, weights
+end
+
+# special methods of GaussRadauLeft for SymPy and SymEngine are in __init__
+
+function Base.show(io::IO, basis::GaussRadauLeft{T}) where {T}
+  print(io, "GaussRadauLeft{", T, "}: Nodal left Gauss Radau basis of degree ",
+            degree(basis))
+end
+
+@inline includes_boundaries(basis::GaussRadauLeft) = Val{false}()
+@inline includes_left_boundary(basis::GaussRadauLeft) = Val{true}()
+@inline includes_right_boundary(basis::GaussRadauLeft) = Val{false}()
+
+@inline satisfies_sbp(basis::GaussRadauLeft) = Val{true}()
+
+
+"""
+    GaussRadauRight{T}
+
+The nodal basis corresponding to Radau Gauss quadrature in [-1,1]
+including the right end point with scalar type `T`.
+"""
+@auto_hash_equals struct GaussRadauRight{T} <: NodalBasis{Line}
+    nodes::Vector{T}
+    weights::Vector{T}
+    baryweights::Vector{T}
+    D::Matrix{T}
+    interp_left::Vector{T}
+    interp_right::Vector{T}
+
+    function GaussRadauRight(nodes::Vector{T}, weights::Vector{T}, baryweights::Vector{T}, D::Matrix{T}, interp_left::Vector{T}, interp_right::Vector{T}) where {T}
+        @argcheck length(nodes) == length(weights) == length(baryweights) == size(D,1) == size(D,2) == length(interp_left) == length(interp_right)
+        new{T}(nodes, weights, baryweights, D, interp_left, interp_right)
+    end
+end
+
+"""
+    GaussRadauRight(p::Int, T=Float64)
+
+Generate the `GaussRadauRight` basis of degree `p` with scalar type `T`.
+"""
+function GaussRadauRight(p::Int, T=Float64)
+    nodes, weights = gauss_radau_nodes_and_weights_right_impl(p, T)
+    baryweights = barycentric_weights(nodes)
+    D = derivative_matrix(nodes, baryweights)
+    R = interpolation_matrix(T[-1, 1], nodes, baryweights)
+    GaussRadauRight(nodes, weights, baryweights, D, R[1,:], R[2,:])
+end
+
+function gauss_radau_nodes_and_weights_right_impl(p, T::DataType)
+    nodes::Vector{T}, weights::Vector{T} = gaussradau(p+1, T)
+    # `gaussradau` returns the nodes in [-1, 1] always including the left end point,
+    # so we can reverse the weights and negative nodes to include the right end point
+    -reverse(nodes), reverse(weights)
+end
+
+# special methods of GaussRadauRight for SymPy and SymEngine are in __init__
+
+function Base.show(io::IO, basis::GaussRadauRight{T}) where {T}
+  print(io, "GaussRadauRight{", T, "}: Nodal right Gauss Radau basis of degree ",
+            degree(basis))
+end
+
+@inline includes_boundaries(basis::GaussRadauRight) = Val{false}()
+@inline includes_left_boundary(basis::GaussRadauRight) = Val{false}()
+@inline includes_right_boundary(basis::GaussRadauRight) = Val{true}()
+
+@inline satisfies_sbp(basis::GaussRadauRight) = Val{true}()
 
 
 """
@@ -294,6 +404,8 @@ function Base.show(io::IO, basis::GaussJacobi{T1, T}) where {T1, T}
 end
 
 @inline includes_boundaries(basis::GaussJacobi) = Val{false}()
+@inline includes_left_boundary(basis::GaussJacobi) = Val{false}()
+@inline includes_right_boundary(basis::GaussJacobi) = Val{false}()
 
 @inline satisfies_sbp(basis::GaussJacobi) = Val{false}()
 
@@ -351,6 +463,8 @@ function ClosedNewtonCotes(p::Int, T=Float64)
 end
 
 @inline includes_boundaries(basis::ClosedNewtonCotes) = Val{true}()
+@inline includes_left_boundary(basis::ClosedNewtonCotes) = Val{true}()
+@inline includes_right_boundary(basis::ClosedNewtonCotes) = Val{true}()
 
 @inline satisfies_sbp(basis::ClosedNewtonCotes) = Val{false}()
 
@@ -405,5 +519,7 @@ function ClenshawCurtis(p::Int, T=Float64)
 end
 
 @inline includes_boundaries(basis::ClenshawCurtis) = Val{true}()
+@inline includes_left_boundary(basis::ClenshawCurtis) = Val{true}()
+@inline includes_right_boundary(basis::ClenshawCurtis) = Val{true}()
 
 @inline satisfies_sbp(basis::ClenshawCurtis) = Val{false}()
