587bf3b57ba92fadd23eb1d189fbcbd71439bc07
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-06-04 22:36:52 +0200

    reintroduce functions and use Gamma.jl

--- src/PolynomialBases.jl
+++ src/PolynomialBases.jl
@@ -7,6 +7,7 @@ using AutoHashEquals: @auto_hash_equals
 using FFTW: FFTW
 using FastGaussQuadrature: FastGaussQuadrature, gausslegendre, gausslobatto, gaussjacobi, gaussradau
 using SimpleUnPack: @unpack
+using Gamma: gamma
 
 
 # types
