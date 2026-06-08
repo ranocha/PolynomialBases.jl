918f29ed7cda5db1be81feef02044d351ccc86be
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-06-08 21:42:24 +0200

    promote alpha and beta to T

--- test/jacobi_test.jl
+++ test/jacobi_test.jl
@@ -23,8 +23,7 @@ end
 @inferred jacobi(10., 4, 3, 2)
 
 # Gauss Jacobi nodes and weights
-# TODO: Why do we need such a high tolerance for BigFloat?
-for (T, tol) in ((Float32, 1.0e-2), (Float64, 1.0e-11), (BigFloat, 1.0e-11))
+for (T, tol) in ((BigFloat, 1.0e-50),)
     for p in 0:10, α in range(-0.9, stop=4, length=10), β in range(-0.9, stop=4, length=10)
         x1, w1 = gaussjacobi(p+1, T(α), T(β))
         @inferred PolynomialBases.gauss_jacobi_nodes_and_weights(p, α, β, T)
